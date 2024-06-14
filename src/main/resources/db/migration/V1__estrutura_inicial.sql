127e30e73b709efde64502ce3bb55be0666022dc
Alyson Rodrigo <g.dasilva@hotmail.com>
2024-06-14 00:51:45 -0300

    Ajuste arquivos properties

--- src/main/resources/db/migration/V1__estrutura_inicial.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE IF NOT EXISTS public.category_id_seq;
-
-CREATE TABLE IF NOT EXISTS public.category (
-    id INTEGER PRIMARY KEY DEFAULT NEXTVAL('public.category_id_seq'),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    status BOOLEAN DEFAULT TRUE
-);
